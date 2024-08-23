adb1d228e2055b2a68190a590f19d9baddf1c28e
otabekpanoev01 <otabekpanoev01@gmail.com>
2024-08-23 19:59:00 +0500

    initial classes for category crud

---
+++ src/main/java/uz/pdp/app_codingbat/controller/CategoryController.java
@@ -0,0 +1,16 @@
+package uz.pdp.app_codingbat.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.pdp.app_codingbat.config.core.BaseURI;
+
+
+@RequestMapping(BaseURI.API1 + BaseURI.CATEGORY)
+@RestController
+public class CategoryController {
+
+
+
+}
---
+++ src/main/java/uz/pdp/app_codingbat/service/CategoryService.java
@@ -0,0 +1,11 @@
+package uz.pdp.app_codingbat.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+
+}
